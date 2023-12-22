e8bd6a9124a92df124106042f2431a3c1dcdb9d9
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2023-12-22 20:49:20 +0100

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,33 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim"
+		}
+	}
+})
+
+require("neo-tree").setup {
+	filesystem = {
+		filtered_items = {
+			visible = true
+		}
+	}
+}
+
+vim.cmd[[ set background=dark ]]
